ddbeace62a2f24c7168e7e9d9fe5200e505bab35
Natsu718 <o.natsumi1216@gmail.com>
2024-07-21 16:26:52 +0900

    カリキュラム8-2

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,8 +7,8 @@ use App\Models\Post;
 
 class PostController extends Controller
 {
-    public function index(Post $post)//インポートしたPostをインスタンス化して$postとして使用。
+    public function index(Post $post)
     {
-        return $post->get();//$postの中身を戻り値にする。
+       return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
     }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    
+    public function getByLimit(int $limit_count = 1)
+    {
+        // updated_atで降順に並べたあと、limitで件数制限をかける
+        return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
+    }
+    
+    public function getPaginateByLimit(int $limit_count = 1)
+    {
+        // updated_atで降順に並べたあと、limitで件数制限をかける
+        return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+    }
 }
